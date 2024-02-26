b4ab5343bd4689bb7a7a42d4ab9099219727ee05
José Cleiton <44325413+jcleitonss@users.noreply.github.com>
2024-02-25 22:45:46 -0300

    Update Criptos.js

--- src/View/Criptos.js
+++ src/View/Criptos.js
@@ -4,7 +4,7 @@ import List from "./Components/List";
 
 
 
-export default function Criptos({navigation}) {
+export default function Home({navigation}) {
     
 const [Data, setData] = useState(null);
 
